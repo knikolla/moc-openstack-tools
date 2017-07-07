d394626211fd58ab499edfc7819fc660f6f7af6e
kamfonik <kamfonik@bu.edu>
2017-07-07 00:01:06 -0400

    add helpdesk interface files

---
+++ helpdesk/bash_profile.sh
@@ -0,0 +1,29 @@
+# script aliases for helpdesk user
+
+if [ "$(id -gn)" == "helpdesk" ]; then
+    echo "Welcome to the MOC helpdesk interface.  Type \`moc help\` for instructions."
+   
+    SCRIPT_DIR='/usr/local/src/moc-tools/engage1'
+ 
+    moc() {
+        if [[ $@ == "help" ]]; then
+            command sudo -u moc-tools cat "$SCRIPT_DIR/helpdesk/help.txt" | more
+        elif [[ $@ =~ ^reset-password ]]; then
+            shift 1
+            command sudo -u moc-tools python "$SCRIPT_DIR/helpdesk/reset-password.py" $@
+        elif [[ $@ =~ ^grant-access ]]; then
+            shift 1
+            command sudo -u moc-tools python "$SCRIPT_DIR/addusers.py" $@
+        elif [[ $@ =~ ^update-quotas ]]; then
+            shift 1
+            command sudo -u moc-tools python "$SCRIPT_DIR/set-quotas.py" $@
+# This can be uncommented for testing updates to this script without interacting with OpenStack
+#        elif [[ $@ == "test" ]]; then
+#            command sudo -u moc-tools python "$SCRIPT_DIR/helpdesk/test.py
+        else
+            echo "$@ is not valid input.  Type 'moc help' to see a list of available commands."
+        fi
+    }
+
+fi
+
